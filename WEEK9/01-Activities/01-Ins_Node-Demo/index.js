cc12a30d466d8abd40bf249f2c849dff9e81e848
Aisha Abdi <aishahajisirad@gmail.com>
2022-05-26 20:12:26 +0100

    git commit

--- WEEK9/01-Activities/01-Ins_Node-Demo/index.js
+++ WEEK9/01-Activities/01-Ins_Node-Demo/index.js
@@ -3,9 +3,10 @@ const displayMessage = function (message) {
   console.log(message)
   };
   
-  displayMessage( 'Hellooo, Node!');
+  displayMessage('Hellooo, Node!');
   
   /*
   click on 'open in intergrated terminal' 
   after you do this type in node ./index.js
   Your terminal will then display the string
+  */
