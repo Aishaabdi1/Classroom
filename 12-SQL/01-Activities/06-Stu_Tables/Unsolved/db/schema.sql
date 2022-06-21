6945f9f580218a06cbb02b376a14620e9445a45e
Aisha Abdi <aishahajisirad@gmail.com>
2022-06-21 21:04:21 +0100

    git commit

--- 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -3,5 +3,11 @@ DROP DATABASE IF EXISTS books_db;
 CREATE DATABASE books_db;
 USE books_db;
 
--- How to add to a table into a specific databse --
+-- How to add to a table into a specific databse -- 
+
 -- Add biography table --
+CREATE TABLE biographies (
+    id INT 
+    name VARCHAR(100)
+
+); 
