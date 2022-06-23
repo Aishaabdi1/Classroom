8f3285abdd55db4e167faf688820b4271f46e676
Aisha Abdi <aishahajisirad@gmail.com>
2022-06-23 20:45:40 +0100

    git commit

--- 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,19 @@
--- Write your schema here --
+DROP DATABASE IF EXISTS schema_db;
+CREATE DATABASE schema_db;
+
+USE schema_db;
+
+
+CREATE TABLE products (
+  id INT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  category_name VARCHAR(30) NOT NULL,
+  price INT NOT NULL,
+  in_stock BOOLEAN NOT NULL
+);
+
+
+CREATE TABLE categories (
+  id INT NOT NULL,
+  category_name VARCHAR(30) NOT NULL,
+);
