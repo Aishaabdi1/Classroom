2f6f309637e95402aae79e18cd0353948a789727
Aisha Abdi <aishahajisirad@gmail.com>
2022-06-23 12:06:36 +0100

    git commit

--- 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
+++ 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,9 @@
 -- Write code here or enter directly in MySQL shell --
+
+-- Delete database --
+DROP DATABASE IF EXISTS sample_db;
+DROP DATABASE IF EXISTS books_db;
+-- Create database --
+CREATE DATABASE books_db;
+-- Select database --
+USE books_db;
