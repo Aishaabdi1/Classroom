fdee7e47fe382a50608adb47bd29579df5f9a81d
Aisha Abdi <aishahajisirad@gmail.com>
2022-07-02 10:49:40 +0100

    commit

---
+++ WEEK12 SQL/01-Activities/20-Stu_Foreign-Primary-Key/Unsolved/db/schema.sql
@@ -0,0 +1,22 @@
+-- Write your Schema Here -- 
+
+DROP DATABASE IF EXISTS registrar_db;
+CREATE DATABASE registrar_db;
+
+USE registrar_db;
+
+CREATE TABLE customers (
+  id INT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE customer_order (
+  id INT,
+  customer_id INT NOT NULL,
+  order_details TEXT,
+  FOREIGN KEY (customer_id)
+  REFERENCES customers(id)
+  ON DELETE SET NULL
+);
